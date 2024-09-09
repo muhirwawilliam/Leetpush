40448352d9facc6942391610977bdd803d292597
Muhirwa William <139078283+muhirwawilliam@users.noreply.github.com>
2024-09-09 16:56:13 +0200

    [2626] [Time Beats: 17.59%] [Memory Beats: 74.22%] - LeetPush

--- 2626-Array-Reduce-Transformation.js
+++ 2626-Array-Reduce-Transformation.js
@@ -5,9 +5,9 @@
  * @return {number}
  */
 var reduce = function(nums, fn, init) {
-    let val = init; 
-    for (let i = 0; i < nums.length; i++) {
-       val = fn(val, nums[i]); 
+    let val = init;
+    for(i = 0; i < nums.length; i++ ) {
+        val = fn(val, nums[i]);
     }
     return val; 
 };
